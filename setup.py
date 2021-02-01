af3b9163a3ee625b96fb0b07f7948be01a07829f
yilin yang <yiliny2@andrew.cmu.edu>
2021-02-01 13:36:26 -0500

    fix

--- setup.py
+++ setup.py
@@ -4,13 +4,12 @@ from setuptools import setup, find_packages
 setup(
     name="nn_optimizer",
     version="0.1",
-    description="NN active learning for molecular geometry optimization",
-    url="https://github.com/yilinyang1/NN-ensemble-relaxer",
+    description="NN active learning for molecular geometry optimization.",
+    url="https://github.com/yilinyang1/nn_optimizer",
     author="Yilin Yang",
-    author_email="ruiqic@andrew.cmu.edu, madams2@andrew.cmu.edu",
+    author_email="yiliny2@andrew.cmu.edu",
     packages=find_packages(),
     include_package_data=True,
     install_requires=["ase"],
-    long_description="""Module for performing delta active learning \
-                          with machine learning potentials.""",
+    long_description="""NN active learning for molecular geometry optimization.""",
 )
